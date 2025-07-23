b00ae7fcc03db6dd37650149d652fc50294ea22f
Piyali Jina <piyallijina@gmail.com>
2025-07-23 14:46:14 -0700

    I made a button and a text box

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is your name?")
+            TextField("Type your name here:",
+                text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            .multilineTextAlignment(.center)
+            .font(.title)
+            .border(Color.black, width: 2)
+            .padding()
+            Button("Submit name here") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }//closes button
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(Color(hue: 0.68, saturation: 0.361, brightness: 0.498))
         }
         .padding()
     }
